5672beefddf8f7c734e72305e3618dda897892ca
Reto Kaiser <reto@cargomedia.ch>
2013-12-06 17:24:29 +0100

    Implement memcache client

--- lib/plugin/memcached.rb
+++ lib/plugin/memcached.rb
@@ -1,11 +1,30 @@
+require 'memcached'
+class MemcachedClient < Memcached
+end
+
 module CoppereggAgents
 
   class Plugin::Memcached < Plugin
 
+
     def monitor(server)
-      raise 'foo'
       p server
-      {:requests => 12}
+      cache = MemcachedClient.new(server['hostname'] + ':' + server['port'].to_s)
+      stats = cache.stats
+
+      keys = [
+          :cmd_get,
+          :cmd_set,
+          :get_misses,
+          :bytes,
+          :limit_maxbytes,
+      ]
+
+      data = {}
+      keys.each { |key|
+        data[key] = stats[key].shift.to_i
+      }
+      data
     end
   end
 end
