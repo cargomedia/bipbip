37386affe6b61613a349190262e1e3668ccf06ff
Reto Kaiser <reto@cargomedia.ch>
2013-12-12 17:25:40 +0100

    Assert that thread still runs at end of test

--- spec/bipbip/agent_spec.rb
+++ spec/bipbip/agent_spec.rb
@@ -3,16 +3,16 @@ require 'bipbip'
 describe Bipbip::Agent do
   let(:agent) { Bipbip::Agent.new }
 
-  it 'should run' do
+  it 'should run and warn' do
     Bipbip.logger = double('logger')
 
     Bipbip.logger.should_receive(:info).with('Startup...')
     Bipbip.logger.should_receive(:warn).with('No services configured')
     Bipbip.logger.should_receive(:warn).with('No storages configured')
 
-    Thread.new do
-      agent.run
-    end
+    thread = Thread.new { agent.run }
     sleep 0.1
+
+    thread.alive?.should == true
   end
 end
