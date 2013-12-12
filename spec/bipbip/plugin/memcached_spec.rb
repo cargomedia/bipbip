a1763f7d936a2c50f5f5bc81ce1f441f60c7c83f
Reto Kaiser <reto@cargomedia.ch>
2013-12-12 18:41:29 +0100

    Add memcached spec

---
+++ spec/bipbip/plugin/memcached_spec.rb
@@ -0,0 +1,16 @@
+require 'bipbip'
+require 'bipbip/plugin/memcached'
+
+describe Bipbip::Plugin::Memcached do
+  let(:plugin) { Bipbip::Plugin::Memcached.new('memcached', {'hostname' => 'localhost', 'port' => 11211}, 10) }
+
+  it 'should collect data' do
+    data = plugin.monitor
+
+    data['cmd_get'].should be_instance_of(Fixnum)
+    data['cmd_set'].should be_instance_of(Fixnum)
+    data['get_misses'].should be_instance_of(Fixnum)
+    data['limit_maxbytes'].should be_instance_of(Fixnum)
+    data['bytes'].should be_instance_of(Fixnum)
+  end
+end
