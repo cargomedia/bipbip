c6b505260e878037f69606d28acf2daa40a7775c
Reto Kaiser <reto@cargomedia.ch>
2013-12-12 17:16:36 +0100

    Simplify load_config by using a default hash

--- lib/bipbip/agent.rb
+++ lib/bipbip/agent.rb
@@ -38,46 +38,35 @@ module Bipbip
 
     def load_config(config_file)
       config = YAML.load(File.open(config_file))
+      config = {
+          'logfile' => STDOUT,
+          'loglevel' => 'INFO',
+          'frequency' => 60,
+          'include' => nil,
+          'services' => [],
+          'services' => [],
+      }.merge(config)
 
-      logfile = STDOUT
-      if config.has_key?('logfile')
-        logfile = config['logfile'].to_s
-      end
-      loglevel = 'INFO'
-      if config.has_key?('loglevel')
-        loglevel = config['loglevel'].to_s
-      end
-      Bipbip.logger = Logger.new(logfile)
-      Bipbip.logger.level = Logger::const_get(loglevel)
+      Bipbip.logger = Logger.new(config['logfile'])
+      Bipbip.logger.level = Logger::const_get(config['loglevel'])
 
-      frequency = 60
-      if config.has_key?('frequency')
-        frequency = config['frequency'].to_i
-      end
-
-      services = []
-      if config.has_key?('services')
-        services += config['services'].to_a
-      end
-      if config.has_key?('include')
+      services = config['services'].to_a
+      if config['include']
         include_path = File.expand_path(config['include'].to_s, File.dirname(config_file))
         files = Dir[include_path + '/**/*.yaml', include_path + '/**/*.yml']
         services += files.map { |file| YAML.load(File.open(file)) }
       end
       @plugins = services.map do |service|
-        name = service['plugin'].to_s
-        config = service.reject { |key, value| ['plugin'].include?(key) }
-        Bipbip::Plugin.factory(name, config, frequency)
+        service_name = service['plugin'].to_s
+        service_config = service.reject { |key, value| ['plugin'].include?(key) }
+        Bipbip::Plugin.factory(service_name, service_config, config['frequency'])
       end
 
-      storages = []
-      if config.has_key?('storages')
-        storages += config['storages'].to_a
-      end
+      storages = config['storages'].to_a
       @storages = storages.map do |storage|
-        name = storage['name'].to_s
-        config = storage.reject { |key, value| ['name'].include?(key) }
-        Bipbip::Storage.factory(name, config)
+        storage_name = storage['name'].to_s
+        storage_config = storage.reject { |key, value| ['name'].include?(key) }
+        Bipbip::Storage.factory(storage_name, storage_config)
       end
     end
 
