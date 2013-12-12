e46f5fab6a04367c2c284144d890e3bbd2ec0daf
Reto Kaiser <reto@cargomedia.ch>
2013-12-12 10:22:50 +0100

    Add spec without api key

--- spec/bipbip/agent_spec.rb
+++ spec/bipbip/agent_spec.rb
@@ -1,4 +1,11 @@
-require 'bipbip/agent'
+require 'bipbip'
 
 describe Bipbip::Agent do
+  let(:agent) { Bipbip::Agent.new }
+
+  it 'should exit without api key' do
+    lambda {
+      agent.run
+    }.should raise_error SystemExit
+  end
 end
