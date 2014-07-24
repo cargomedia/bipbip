ca104c354a12853ae985d5881dbceff4f00ae0f8
ppp0 <philipp.gysin@cargomedia.ch>
2014-07-24 18:18:53 +0200

    Adjusting mongodb arguments

--- spec/bipbip/plugin/mongodb_spec.rb
+++ spec/bipbip/plugin/mongodb_spec.rb
@@ -2,7 +2,7 @@ require 'bipbip'
 require 'bipbip/plugin/mongodb'
 
 describe Bipbip::Plugin::Mongodb do
-  let(:plugin) { Bipbip::Plugin::Mongodb.new('mongodb', {'host' => 'mongo.rep2-db1', 'port' => 27018}, 10) }
+  let(:plugin) { Bipbip::Plugin::Mongodb.new('mongodb', {'host' => 'localhost', 'port' => 27017}, 10) }
 
   it 'should collect data' do
     data = plugin.monitor
