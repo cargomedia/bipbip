4e285c455e49256cb2ab34bb13838e23e1205552
Chris Stasiak <me@krzysztof-stasiak.com>
2014-10-03 11:05:18 +0200

    File seeking; removing first line; adjusting offset

--- lib/bipbip/plugin/log_parser.rb
+++ lib/bipbip/plugin/log_parser.rb
@@ -2,11 +2,12 @@ require 'uri'
 
 module Bipbip
 
+  TIMESTAMP_REGEXP = '^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}\b'
+
   class Plugin::LogParser < Plugin
 
     def initialize(name, config, frequency)
-      @start_at = Time.now - frequency.to_i
-      @source_uri = nil
+      @start_timestamp = Time.now - frequency.to_i
 
       super name, config, frequency
     end
@@ -36,7 +37,7 @@ module Bipbip
     def source_uri
       @source_uri ||= [0].tap { |u|
         u[0] = URI(config['uri'])
-        raise 'URI is not valid' unless ['file'].include? u[0].scheme
+        raise 'URI is not valid. Supported uri `file` type only.' unless ['file'].include? u[0].scheme
       }.first
     end
 
@@ -54,44 +55,47 @@ module Bipbip
 
     def _file_get_lines
       raise 'File does not exist' unless File.exists?(source_path)
-      raise 'Regexp for timestamp is not set' unless config.key?('regexp_timestamp')
 
-      regexp_timestamp = Regexp.new(config['regexp_timestamp'])
+      regexp_timestamp = config.key?('regexp_timestamp') ? Regexp.new(config['regexp_timestamp']) : TIMESTAMP_REGEXP
 
       [].tap do |b|
 
-        buffer_size =  1 << 16
-        start_timestamp = @start_at.strftime("%FT%T")
+        buffer_size =  1 << 6
+        start_timestamp = @start_timestamp.strftime("%FT%T")
 
         File.open(source_path) do |file|
           if file.stat.size < buffer_size
             buffer_size = file.stat.size
           end
 
-          file.seek(-buffer_size, File::SEEK_END)
-          seek_position = buffer_size
-
-          # make resistant for line braking
+          seek_position = -buffer_size
+          file.seek(seek_position, File::SEEK_END)
 
           while seek_position <= file.stat.size
             buffer = file.read(buffer_size)
             line_list = buffer.split("\n")
 
+            # Remove first line as can be incomplete
+            # due to seeking backward with
+            # buffer_size steps
+            first_line = line_list.shift
+            offset = first_line.length
+
             line_count = b.length
             line_list.each do |line|
               if (t = line.match(regexp_timestamp)) && (t[0] > start_timestamp)
                 line_timestamp = DateTime.parse(t[0])
-                @start_at = line_timestamp if line_timestamp > @start_at
-
+                @start_timestamp = line_timestamp if line_timestamp > @start_timestamp
                 b.push line
               end
             end
 
             break if line_count == b.length
 
-            seek_position = seek_position + buffer_size
-            break if seek_position > file.stat.size
-            file.seek(2 * -buffer_size, File::SEEK_CUR)
+            seek_position = seek_position - buffer_size + offset
+            break if 0 > file.stat.size + seek_position
+
+            file.seek(2 * -buffer_size + offset, File::SEEK_CUR)
           end
 
           file.close
