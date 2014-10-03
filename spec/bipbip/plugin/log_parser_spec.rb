56b7889653fcdeddede4fc6af339f637ed51da27
Chris Stasiak <me@krzysztof-stasiak.com>
2014-10-03 11:05:27 +0200

    Adjusted tests

--- spec/bipbip/plugin/log_parser_spec.rb
+++ spec/bipbip/plugin/log_parser_spec.rb
@@ -3,7 +3,7 @@ require 'bipbip/plugin/log_parser'
 
 describe Bipbip::Plugin::LogParser do
 
-  let(:plugin) { Bipbip::Plugin::LogParser.new('log-parser', {
+  let(:plugin1) { Bipbip::Plugin::LogParser.new('log-parser', {
       'name' => 'oom_killer_activity',
       'uri' => 'file://localhost' + File.expand_path('../../../testdata/sample_logs/sample.log', __FILE__),
       'regexp_text' => 'oom_killer',
@@ -11,10 +11,23 @@ describe Bipbip::Plugin::LogParser do
     }, 10)
   }
 
-  it 'should collect data' do
-    data = plugin.monitor
+  let(:plugin2) { Bipbip::Plugin::LogParser.new('log-parser', {
+      'name' => 'root_activity',
+      'uri' => 'file://localhost' + File.expand_path('../../../testdata/sample_logs/sample.log', __FILE__),
+      'regexp_text' => 'root.*login$'
+  }, 10)
+  }
 
-    data['inactive_oom_killer'].should be_instance_of(Fixnum)
-    data['inactive_oom_killer'].should eq(4)
+  it 'should collect data' do
+    data1 = plugin1.monitor
+    data2 = plugin2.monitor
+
+    data = data1.merge(data2)
+
+    data['oom_killer_activity'].should be_instance_of(Fixnum)
+    data['oom_killer_activity'].should eq(7)
+
+    data['root_activity'].should be_instance_of(Fixnum)
+    data['root_activity'].should eq(1)
   end
 end
