fa9984b621443fab00a05864d105e32a384d7741
Reto Kaiser <reto@cargomedia.ch>
2014-10-13 15:38:13 +0200

    Split "reading lines backwards" and "parsing lines with timestamp" into two functions

--- lib/bipbip/plugin/log_parser.rb
+++ lib/bipbip/plugin/log_parser.rb
@@ -21,6 +21,18 @@ module Bipbip
     private
 
     def match_count
+      time_first = nil
+
+      lines = lines_backwards.take_while do |line|
+        timestamp_match = line.match(regexp_timestamp)
+        raise "Line doesn't match `#{regexp_timestamp}`: `#{line}`" if timestamp_match.nil?
+        time = DateTime.parse(timestamp_match[0]).to_time
+        time_first ||= time
+        (time > log_time_min)
+      end
+
+      self.log_time_min = time_first unless time_first.nil?
+
       lines.reject { |line| line.match(Regexp.new(config['regexp_text'])).nil? }.length
     end
 
@@ -36,53 +48,37 @@ module Bipbip
       @regexp_timestamp ||= Regexp.new(config.fetch('regexp_timestamp', TIMESTAMP_REGEXP))
     end
 
+    def lines_backwards
+      buffer_size = 65536
 
-
-    def lines
-      [].tap do |b|
-
-        buffer_size = 65536
-
+      Enumerator.new do |yielder|
         File.open(config['path']) do |file|
           file.seek(0, File::SEEK_END)
 
-          _time = log_time_min
-          while file.pos >= 0
-            if file.pos < buffer_size
-              buffer_size = file.pos
-            end
+          while file.pos > 0
+            buffer_size = file.pos if file.pos < buffer_size
 
             file.seek(-buffer_size, File::SEEK_CUR)
+            buffer = file.read(buffer_size)
+            file.seek(-buffer_size, File::SEEK_CUR)
 
-            line_list = file.read(buffer_size).split("\n")
+            line_list = buffer.each_line.entries
 
-            # Remove first line as can be incomplete
-            # due to seeking backward with
-            # buffer_size steps
-            first_line = line_list.shift
-            file.seek(-buffer_size + first_line.length, File::SEEK_CUR)
-
-            a = line_list.select do |line|
-              timestamp = line.match(regexp_timestamp)
-              unless timestamp.nil?
-                time = DateTime.parse(timestamp[0]).to_time
-                if time >= log_time_min
-                  1 == 1
-                end
-                if time > _time
-                  _time = time
-                end
-              end
+            if file.pos != 0
+              # Remove first line as can be incomplete
+              # due to seeking backward with buffer_size steps
+              first_line = line_list.shift
+              raise "Line length exceeds buffer size `#{buffer_size}`" if first_line.length == buffer_size
+              file.seek(first_line.length, File::SEEK_CUR)
             end
 
-            b.push(*line_list)
-
-            break if line_list.empty?
+            line_list.reverse.each do |line|
+              yielder.yield(line)
+            end
           end
-
-          self.log_time_min = _time
         end
       end
     end
+
   end
 end
