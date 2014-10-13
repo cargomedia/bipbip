4d65b0b429cbabf40fd2df8275410a170c2c6bb6
ppp0 <philipp.gysin@cargomedia.ch>
2014-10-13 18:18:16 +0200

    Add test for syslog-style file (not suitable for merging, only for testing now - date will make problems in a near future)

--- spec/bipbip/plugin/log_parser_spec.rb
+++ spec/bipbip/plugin/log_parser_spec.rb
@@ -24,24 +24,43 @@ describe Bipbip::Plugin::LogParser do
             },
         ]
     }, 10)
+
+    @plugin3 = Bipbip::Plugin::LogParser.new('log-parser', {
+        'path' => File.expand_path('../../../testdata/sample_logs/syslog-sample.log', __FILE__),
+        'regexp_timestamp' => '\w+ \d{1,2} \d{2}\:\d{2}\:\d{2}',
+        'matchers' => [
+            {
+                'name' => 'segfaults',
+                'regexp' => '.*segfault+.*'
+            },
+        ]
+    }, 10)
   end
 
   it 'should match log entries for first run' do
     data1 = @plugin1.monitor
     data2 = @plugin2.monitor
+    data3 = @plugin3.monitor
 
-    data = data1.merge(data2)
+    data = data1.merge(data2).merge(data3)
 
     data['oom_killer_activity'].should be_instance_of(Fixnum)
     data['oom_killer_activity'].should eq(7)
 
     data['root_activity'].should be_instance_of(Fixnum)
     data['root_activity'].should eq(1)
+
+    data['segfaults'].should be_instance_of(Fixnum)
+    data['segfaults'].should eq(6)
   end
 
   it 'should not match any log entries for second run' do
     data1 = @plugin1.monitor
     data2 = @plugin2.monitor
+    data3 = @plugin3.monitor
+
+    data = data1.merge(data2).merge(data3)
+
 
     data = data1.merge(data2)
 
@@ -50,5 +69,8 @@ describe Bipbip::Plugin::LogParser do
 
     data['root_activity'].should be_instance_of(Fixnum)
     data['root_activity'].should eq(0)
+
+    data['segfaults'].should be_instance_of(Fixnum)
+    data['segfaults'].should eq(0)
   end
 end
