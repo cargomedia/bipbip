c0206645d53aa645b84571651da44bfc8b21ce35
Reto Kaiser <reto@cargomedia.ch>
2014-10-22 15:14:30 +0200

    Use mock plugin in agent spec

--- spec/bipbip/agent_spec.rb
+++ spec/bipbip/agent_spec.rb
@@ -4,7 +4,6 @@ require 'tempfile'
 
 describe Bipbip::Agent do
   let(:agent) { Bipbip::Agent.new }
-  let(:plugin) { Bipbip::Plugin::Memcached.new('memcached', {'hostname' => 'localhost', 'port' => 123}, 10) }
 
   it 'should fail without services' do
     Bipbip.logger = double('logger')
@@ -18,15 +17,22 @@ describe Bipbip::Agent do
   end
 
   it 'should fork' do
-    Bipbip.logger = Logger.new('/dev/null')
-    agent.plugins = [plugin]
+    logger_file = Tempfile.new('bipbip-mock-logger')
+    Bipbip.logger = Logger.new(logger_file.path)
 
-    plugin.should_receive(:run)
+    plugin = Bipbip::Plugin.new('my-plugin', {}, 0.1)
+    plugin.stub(:metrics_schema) { [{:name => 'foo', :type => 'counter'}] }
+    plugin.stub(:source_identifier) { 'my-source' }
+    plugin.stub(:monitor) { {:foo => 12} }
+
+    agent.plugins = [plugin]
 
     thread = Thread.new { agent.run }
     sleep 0.5
 
     thread.alive?.should eq(true)
+    lines = logger_file.read.lines
+    lines.select { |l| l.include?('my-plugin my-source: Data: {:foo=>12}') }.should have_at_least(2).items
 
     Bipbip.logger = nil
   end
