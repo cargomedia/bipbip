845dd4c7114d1afbdd198d192adf2fe405e06934
Chris Stasiak <me@krzysztof-stasiak.com>
2014-11-28 17:06:23 +0100

    Removed test for mongo client connection

--- spec/bipbip/plugin/mongodb_spec.rb
+++ spec/bipbip/plugin/mongodb_spec.rb
@@ -12,14 +12,6 @@ describe Bipbip::Plugin::Mongodb do
 end
 
 describe Bipbip::Plugin::Mongodb do
-  let(:plugin) { Bipbip::Plugin::Mongodb.new('mongodb', {'port' => 22022}, 10) }
-
-  it 'should raise an error' do
-    expect { plugin.monitor }.to raise_error
-  end
-end
-
-describe Bipbip::Plugin::Mongodb do
   let(:plugin) { Bipbip::Plugin::Mongodb.new('mongodb', {'hostname' => 'localhost', 'port' => 27017}, 10) }
 
   it 'should collect replication lag' do
