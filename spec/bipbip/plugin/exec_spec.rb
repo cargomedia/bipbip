d0a05ffb76bea652d08a3b30520add47ce9becf7
Chris Stasiak <me@krzysztof-stasiak.com>
2015-01-13 15:00:53 +0100

    Added spec

---
+++ spec/bipbip/plugin/exec_spec.rb
@@ -0,0 +1,29 @@
+require 'bipbip'
+require 'bipbip/plugin/exec'
+
+describe Bipbip::Plugin::Exec do
+  let(:plugin) { Bipbip::Plugin::Exec.new(
+      'exec', {'command' => 'pulsar -c /Users/cargomedia/Projects/pulsar-conf-cargomedia sk production mongo:health:check -s json_print=true -l 0', 'type' => 'gauge', 'unit' => 'Boolean'}, 10) }
+
+  it 'should collect data' do
+
+    plugin.stub(:exec_command).and_return(
+<<DATA
+{
+  "common_ok": true,
+  "router_ok": false,
+  "config_ok": true,
+  "replica_ok": true,
+  "mms_ok": true,
+  "cluster_ok": false
+}
+DATA
+    )
+
+    data = plugin.monitor
+
+    data['common_ok'].should eq(1)
+    data['router_ok'].should eq(0)
+    data['cluster_ok'].should eq(0)
+  end
+end
