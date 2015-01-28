7d25f862899303b5672f21a571e3d5202eee8c18
Chris Stasiak <me@krzysztof-stasiak.com>
2015-01-28 10:53:40 +0100

    Added socket-redis plugin

---
+++ lib/bipbip/plugin/socket_redis.rb
@@ -0,0 +1,33 @@
+module Bipbip
+
+  class Plugin::SocketRedis < Plugin
+
+    def metrics_schema
+      [
+          {:name => 'channels_count', :type => 'gauge', :unit => 'Channels'},
+          {:name => 'subscribers_count', :type => 'gauge', :unit => 'Subscribers'},
+      ]
+    end
+
+    def monitor
+      stats = fetch_socket_redis_stats
+      {
+          'channels_count' => stats.length,
+          'subscribers_count' => stats.values.reduce(0) { |memo, channel| memo += channel['subscribers'].length },
+      }
+    end
+
+    private
+
+    def fetch_socket_redis_stats
+      url = config['url'] || 'http://localhost:8085/status'
+      uri = URI.parse(url)
+
+      response = Net::HTTP.get_response(uri)
+      raise "Invalid response from server at #{config['url']}" unless response.code == '200'
+
+      JSON.parse(response.body)
+    end
+
+  end
+end
