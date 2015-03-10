1126d0288afa357bbcfb5f726ed519470ec36f1d
Reto Kaiser <reto@cargomedia.ch>
2015-03-10 09:20:55 +0100

    Improve storage logging

--- lib/bipbip/storage/copperegg.rb
+++ lib/bipbip/storage/copperegg.rb
@@ -12,28 +12,29 @@ module Bipbip
       @dashboards ||= _load_dashboards
 
       if ![5, 15, 60, 300, 900, 3600, 21600].include?(plugin.frequency)
-        Bipbip.logger.fatal "Copperegg cannot use frequency #{plugin.frequency}"
+        log(Logger::FATAL, "Cannot use frequency #{plugin.frequency}")
         exit 1
       end
 
       metric_group = @metric_groups.detect { |m| m.name == plugin.metric_group }
       if metric_group.nil? || !metric_group.is_a?(::Copperegg::Revealmetrics::MetricGroup)
-        Bipbip.logger.info "Creating copperegg metric group `#{plugin.metric_group}`"
+        log(Logger::INFO, "Creating metric group `#{plugin.metric_group}`")
         metric_group = ::Copperegg::Revealmetrics::MetricGroup.new(:name => plugin.metric_group, :label => plugin.metric_group, :frequency => plugin.frequency)
       end
       metric_group.frequency = plugin.frequency
       metric_group.metrics = plugin.metrics_schema.map do |sample|
         {
-            :name => sample[:name],
-            :type => 'ce_' + sample[:type],
-            :unit => sample[:unit],
+          :name => sample[:name],
+          :type => 'ce_' + sample[:type],
+          :unit => sample[:unit],
         }
       end
+      log(Logger::INFO, "Updating metric group `#{plugin.metric_group}`")
       metric_group.save
 
       dashboard = @dashboards.detect { |d| d.name == plugin.metric_group }
       if dashboard.nil?
-        Bipbip.logger.info "Creating copperegg dashboard `#{plugin.metric_group}`"
+        log(Logger::INFO, "Creating dashboard `#{plugin.metric_group}`")
         metrics = metric_group.metrics || []
         ::Copperegg::Revealmetrics::CustomDashboard.create(metric_group, :name => plugin.metric_group, :identifiers => nil, :metrics => metrics)
       end
@@ -47,20 +48,20 @@ module Bipbip
     end
 
     def _load_metric_groups
-      Bipbip.logger.info 'Loading copperegg metric groups'
+      log(Logger::INFO, 'Loading metric groups')
       metric_groups = ::Copperegg::Revealmetrics::MetricGroup.find
       if metric_groups.nil?
-        Bipbip.logger.fatal 'Cannot load copperegg metric groups'
+        log(Logger::FATAL, 'Cannot load metric groups')
         exit 1
       end
       metric_groups
     end
 
     def _load_dashboards
-      Bipbip.logger.info 'Loading copperegg dashboards'
+      log(Logger::INFO, 'Loading dashboards')
       dashboards = ::Copperegg::Revealmetrics::CustomDashboard.find
       if dashboards.nil?
-        Bipbip.logger.fatal 'Cannot load copperegg dashboards'
+        log(Logger::FATAL, 'Cannot load dashboards')
         exit 1
       end
       dashboards
