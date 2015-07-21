de5e4798f5a4b575cfdc4708ad3ea48f47ea3ae2
Reto Kaiser <reto@cargomedia.ch>
2015-07-21 11:00:11 +0200

    Add more nginx metrics

--- lib/bipbip/plugin/nginx.rb
+++ lib/bipbip/plugin/nginx.rb
@@ -4,7 +4,14 @@ module Bipbip
 
     def metrics_schema
       [
-          {:name => 'connections_requested', :type => 'counter', :unit => 'Requests'},
+        {:name => 'connections_accepts', :type => 'counter', :unit => 'Connections'},
+        {:name => 'connections_handled', :type => 'counter', :unit => 'Connections'},
+        {:name => 'connections_dropped', :type => 'counter', :unit => 'Connections'},
+        {:name => 'connections_requests', :type => 'counter', :unit => 'Requests'},
+        {:name => 'active_total', :type => 'gauge', :unit => 'Connections'},
+        {:name => 'active_reading', :type => 'gauge', :unit => 'Connections'},
+        {:name => 'active_writing', :type => 'gauge', :unit => 'Connections'},
+        {:name => 'active_waiting', :type => 'gauge', :unit => 'Connections'},
       ]
     end
 
@@ -14,10 +21,36 @@ module Bipbip
 
       raise "Invalid response from server at #{config['url']}" unless response.code == "200"
 
-      nstats = response.body.split(/\r*\n/)
-      connections_requested = nstats[2].lstrip.split(/\s+/)[2].to_i
+      lines = response.body.split(/\r*\n/)
+      lines.map { |line| line.strip! }
 
-      {:connections_requested => connections_requested}
+      data = {}
+
+      stats_connections = match_or_fail(lines[2], /^(\d+) (\d+) (\d+)$/)
+      data[:connections_accepts] = stats_connections[1].to_i
+      data[:connections_handled] = stats_connections[2].to_i
+      data[:connections_dropped] = data[:connections_accepts] - data[:connections_handled]
+      data[:connections_requests] = stats_connections[3].to_i
+
+      stats_active_total = match_or_fail(lines[0], /^Active connections: (\d+)$/)
+      data[:active_total] = stats_active_total[1].to_i
+
+      stats_active = match_or_fail(lines[3], /^Reading: (\d+) Writing: (\d+) Waiting: (\d+)$/)
+      data[:active_reading] = stats_active[1].to_i
+      data[:active_writing] = stats_active[2].to_i
+      data[:active_waiting] = stats_active[3].to_i
+
+      data
+    end
+
+    # @param [String] string
+    # @param [Regexp] regexp
+    def match_or_fail(string, regexp)
+      match_data = regexp.match(string)
+      if match_data.nil?
+        raise "Data `#{string}` doesn't match pattern `#{regexp}`."
+      end
+      match_data
     end
   end
 end
--- spec/bipbip/plugin/nginx_spec.rb
+++ spec/bipbip/plugin/nginx_spec.rb
@@ -9,20 +9,34 @@ describe Bipbip::Plugin::Nginx do
     response = [
       'Active connections: 11',
       'server accepts handled requests',
-      '621436 621436 671531',
-      'Reading: 0 Writing: 11 Waiting: 0',
+      '621436 621434 671531',
+      'Reading: 1 Writing: 7 Waiting: 3',
     ].join("\n")
     stub_request(:get, 'http://localhost/server-status').to_return(status: 200, body: response)
 
     data = plugin.monitor
 
-    data[:connections_requested].should eq(671531)
+    data[:connections_accepts].should eq(621436)
+    data[:connections_handled].should eq(621434)
+    data[:connections_dropped].should eq(2)
+    data[:connections_requests].should eq(671531)
+
+    data[:active_total].should eq(11)
+    data[:active_reading].should eq(1)
+    data[:active_writing].should eq(7)
+    data[:active_waiting].should eq(3)
   end
 
   it 'should fail on non-200 response' do
     stub_request(:get, 'http://localhost/server-status').to_return(status: 404)
 
     expect { plugin.monitor }.to raise_error(/Invalid response/)
+  end
+
+  it 'should fail on unexpected response' do
+    stub_request(:get, 'http://localhost/server-status').to_return(status: 200, body: 'foo')
+
+    expect { plugin.monitor }.to raise_error(/doesn't match pattern/)
   end
 
 end
