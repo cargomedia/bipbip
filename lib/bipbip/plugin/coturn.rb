8bb7170208484099ec4d949f07eeade7e5346d2b
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-04 12:59:30 +0100

    Added outgoing, incoming bitrate metrics

--- lib/bipbip/plugin/coturn.rb
+++ lib/bipbip/plugin/coturn.rb
@@ -4,7 +4,9 @@ module Bipbip
   class Plugin::Coturn < Plugin
     def metrics_schema
       [
-        { name: 'total_sessions_count', type: 'gauge', unit: 'Sessions' }
+        { name: 'total_sessions_count', type: 'gauge', unit: 'Sessions' },
+        { name: 'total_bitrate_outgoing', type: 'gauge', unit: 'b/s' },
+        { name: 'total_bitrate_incoming', type: 'gauge', unit: 'b/s' }
       ]
     end
 
@@ -17,7 +19,9 @@ module Bipbip
       coturn.close
 
       {
-        'total_sessions_count' => current_sessions.match(/Total sessions: (.*)/)[1].to_i
+        'total_sessions_count' => current_sessions.match(/Total sessions: (.*)/)[1].to_i,
+        'total_bitrate_outgoing' => current_sessions.scan(/ s=(\d),/).flatten.map(&:to_i).reduce(:+) * 8,
+        'total_bitrate_incoming' => current_sessions.scan(/ r=(\d),/).flatten.map(&:to_i).reduce(:+) * 8
       }
     end
   end
