1b672382e907cd959f16c3e091c456049f1d8d3e
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-04 11:27:50 +0100

    RUBOCOP

--- lib/bipbip/plugin/coturn.rb
+++ lib/bipbip/plugin/coturn.rb
@@ -4,14 +4,16 @@ module Bipbip
   class Plugin::Coturn < Plugin
     def metrics_schema
       [
-        {name : 'total_sessions_count', type : 'gauge', unit : 'Sessions'}
+        { name: 'total_sessions_count', type: 'gauge', unit: 'Sessions' }
       ]
     end
 
     def monitor
-      coturn = Net::Telnet::new("Host" => config['hostname'] || "localhost",
-                                "Port" => config['port'] || 5766)
-      current_sessions = coturn.cmd("ps")
+      coturn = Net::Telnet.new(
+        'Host' => config['hostname'] || 'localhost',
+        'Port' => config['port'] || 5766
+      )
+      current_sessions = coturn.cmd('ps')
       coturn.close
 
       {
