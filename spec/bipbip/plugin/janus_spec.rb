203a7f2526899cfaab3e6e3c02dd3873e9a0327e
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-11 15:41:58 +0100

    Added spec

---
+++ spec/bipbip/plugin/janus_spec.rb
@@ -0,0 +1,58 @@
+require 'bipbip'
+require 'bipbip/plugin/janus'
+
+describe Bipbip::Plugin::Janus do
+  let(:plugin) { Bipbip::Plugin::Janus.new('coturn', {'websocket_url' => 'ws://10.10.10.111:8188'}, 10) }
+
+  it 'should collect janus rtpbroadcast status data' do
+    response = {
+      "plugin" => "janus.plugin.cm.rtpbroadcast",
+      "data" => {
+        "streaming" => "list",
+        "list" => [
+          {
+            "id" => "1",
+            "name" => "1",
+            "description" => "Opus/VP8 tester.py test stream",
+            "streams" => [
+              {
+                "id" => "1",
+                "index" => 1,
+                "audioport" => 8784,
+                "videoport" => 8504,
+                "stats" => {"min" => 0.0, "max" => 0.0, "cur" => 0.0, "avg" => 0.0},
+                "frame" => {"width" => 0, "height" => 0, "fps" => 0, "key-distance" => 0},
+                "session" => {"webrtc-active" => 0, "autoswitch-enabled" => 1, "remb-avg" => 0}
+              },
+              {
+                "id" => "1",
+                "index" => 2,
+                "audioport" => 8018,
+                "videoport" => 8624,
+                "stats" => {"min" => 0.0, "max" => 0.0, "cur" => 0.0, "avg" => 0.0},
+                "frame" => {"width" => 0, "height" => 0, "fps" => 0, "key-distance" => 0},
+                "session" => {"webrtc-active" => 0, "autoswitch-enabled" => 1, "remb-avg" => 0}
+              },
+              {
+                "id" => "1",
+                "index" => 3,
+                "audioport" => 8312,
+                "videoport" => 8267,
+                "stats" => {"min" => 0.0, "max" => 0.0, "cur" => 0.0, "avg" => 0.0},
+                "frame" => {"width" => 0, "height" => 0, "fps" => 0, "key-distance" => 0},
+                "session" => {"webrtc-active" => 0, "autoswitch-enabled" => 1, "remb-avg" => 0
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+
+    plugin.stub(:_fetch_rtpbroadcast_data).and_return(response)
+
+    data = plugin.monitor
+
+    data['rtpbroadcast_mountpoints_count'].should eq(1)
+  end
+end
