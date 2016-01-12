fbc3c847d2509a311cda2b9239d8ddce88a94494
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-13 00:35:26 +0100

    Updated spec

--- spec/bipbip/plugin/janus_spec.rb
+++ spec/bipbip/plugin/janus_spec.rb
@@ -2,54 +2,56 @@ require 'bipbip'
 require 'bipbip/plugin/janus'
 
 describe Bipbip::Plugin::Janus do
-  let(:plugin) { Bipbip::Plugin::Janus.new('coturn', {'websocket_url' => 'ws://10.10.10.111:8188'}, 10) }
+  let(:plugin) { Bipbip::Plugin::Janus.new('coturn', {'url' => 'http://10.10.10.111:8088/janus'}, 10) }
 
   it 'should collect janus rtpbroadcast status data' do
-    response = {
-      "plugin" => "janus.plugin.cm.rtpbroadcast",
-      "data" => {
-        "streaming" => "list",
-        "list" => [
+    response = <<EOS
+{
+  "plugin": "janus.plugin.cm.rtpbroadcast",
+  "data": {
+    "streaming": "list",
+    "list": [
+      {
+        "id": "1",
+        "name": "1",
+        "description": "Opus/VP8 tester.py test stream",
+        "streams": [
           {
-            "id" => "1",
-            "name" => "1",
-            "description" => "Opus/VP8 tester.py test stream",
-            "streams" => [
-              {
-                "id" => "1",
-                "index" => 1,
-                "audioport" => 8784,
-                "videoport" => 8504,
-                "stats" => {"min" => 0.0, "max" => 10.0, "cur" => 5.0, "avg" => 5.0},
-                "frame" => {"width" => 0, "height" => 0, "fps" => 10, "key-distance" => 0},
-                "session" => {"webrtc-active" => 0, "autoswitch-enabled" => 1, "remb-avg" => 0}
-              },
-              {
-                "id" => "1",
-                "index" => 2,
-                "audioport" => 8018,
-                "videoport" => 8624,
-                "stats" => {"min" => 0.0, "max" => 20.0, "cur" => 10.0, "avg" => 10.0},
-                "frame" => {"width" => 0, "height" => 0, "fps" => 30, "key-distance" => 0},
-                "session" => {"webrtc-active" => 0, "autoswitch-enabled" => 1, "remb-avg" => 0}
-              },
-              {
-                "id" => "1",
-                "index" => 3,
-                "audioport" => 8312,
-                "videoport" => 8267,
-                "stats" => {"min" => 0.0, "max" => 0.0, "cur" => 0.0, "avg" => 0.0},
-                "frame" => {"width" => 0, "height" => 0, "fps" => 0, "key-distance" => 0},
-                "session" => {"webrtc-active" => 0, "autoswitch-enabled" => 1, "remb-avg" => 0
-                }
-              }
-            ]
+            "id": "1",
+            "index": 1,
+            "audioport": 8784,
+            "videoport": 8504,
+            "stats": {"min": 0.0, "max": 10.0, "cur": 5.0, "avg": 5.0},
+            "frame": {"width": 0, "height": 0, "fps": 10, "key-distance": 0},
+            "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0}
+          },
+          {
+            "id": "1",
+            "index": 2,
+            "audioport": 8018,
+            "videoport": 8624,
+            "stats": {"min": 0.0, "max": 20.0, "cur": 10.0, "avg": 10.0},
+            "frame": {"width": 0, "height": 0, "fps": 30, "key-distance": 0},
+            "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0}
+          },
+          {
+            "id": "1",
+            "index": 3,
+            "audioport": 8312,
+            "videoport": 8267,
+            "stats": {"min": 0.0, "max": 0.0, "cur": 0.0, "avg": 0.0},
+            "frame": {"width": 0, "height": 0, "fps": 0, "key-distance": 0},
+            "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0
+            }
           }
         ]
       }
-    }
+    ]
+  }
+}
+EOS
 
-    plugin.stub(:_fetch_rtpbroadcast_data).and_return(response)
+    plugin.stub(:_fetch_rtpbroadcast_data).and_return(JSON.parse(response))
 
     data = plugin.monitor
 
