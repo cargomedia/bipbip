97141936f1c039d3b50533f97c3f660561a21b1d
Chris Stasiak <me@krzysztof-stasiak.com>
2016-01-20 11:54:32 +0100

    Updated RTP spec

--- spec/bipbip/plugin/janus_rtpbroadcast_spec.rb
+++ spec/bipbip/plugin/janus_rtpbroadcast_spec.rb
@@ -24,7 +24,7 @@ describe Bipbip::Plugin::JanusRtpbroadcast do
             "listeners": 32,
             "waiters": 4,
             "stats": {"min": 0.0, "max": 10.0, "cur": 5.0, "avg": 5.0},
-            "frame": {"width": 0, "height": 0, "fps": 10, "key-distance": 0},
+            "frame": {"width": 0, "height": 0, "fps": 10, "key-distance": 20},
             "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0}
           },
           {
@@ -35,7 +35,7 @@ describe Bipbip::Plugin::JanusRtpbroadcast do
             "listeners": 0,
             "waiters": 12,
             "stats": {"min": 0.0, "max": 20.0, "cur": 10.0, "avg": 10.0},
-            "frame": {"width": 0, "height": 0, "fps": 30, "key-distance": 0},
+            "frame": {"width": 0, "height": 0, "fps": 30, "key-distance": 30},
             "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0}
           },
           {
@@ -46,9 +46,27 @@ describe Bipbip::Plugin::JanusRtpbroadcast do
             "listeners": 88,
             "waiters": 12,
             "stats": {"min": 0.0, "max": 0.0, "cur": 0.0, "avg": 0.0},
-            "frame": {"width": 0, "height": 0, "fps": 0, "key-distance": 0},
+            "frame": {"width": 0, "height": 0, "fps": 0, "key-distance": 40},
             "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0
             }
+          }
+        ]
+      },
+      {
+        "id": "2",
+        "name": "2",
+        "description": "Opus/VP8 tester.py test stream",
+        "streams": [
+          {
+            "id": "2",
+            "index": 1,
+            "audioport": 9784,
+            "videoport": 9504,
+            "listeners": 200,
+            "waiters": 100,
+            "stats": {"min": 0.0, "max": 100.0, "cur": 50.0, "avg": 50.0},
+            "frame": {"width": 0, "height": 0, "fps": 50, "key-distance": 50},
+            "session": {"webrtc-active": 0, "autoswitch-enabled": 1, "remb-avg": 0}
           }
         ]
       }
@@ -57,15 +75,15 @@ describe Bipbip::Plugin::JanusRtpbroadcast do
 }
 EOS
 
-    plugin.stub(:_fetch_rtpbroadcast_data).and_return(JSON.parse(response))
+    plugin.stub(:_fetch_data).and_return(JSON.parse(response))
 
     data = plugin.monitor
 
-    data['mountpoint_count'].should eq(1)
-    data['stream_count'].should eq(3)
-    data['streams_listener_count'].should eq(120)
-    data['streams_waiter_count'].should eq(28)
-    data['streams_bandwidth'].should eq(15)
+    data['mountpoint_count'].should eq(2)
+    data['stream_count'].should eq(4)
+    data['streams_listener_count'].should eq(320)
+    data['streams_waiter_count'].should eq(128)
+    data['streams_bandwidth'].should eq(65)
     data['streams_zero_fps_count'].should eq(1)
     data['streams_zero_bitrate_count'].should eq(1)
   end
