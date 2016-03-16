67ed755a43a231879625ff129fc11bfc2c4f3dfd
Reto Kaiser <reto@cargomedia.ch>
2016-03-16 10:22:31 +0100

    Add "jobs_active_total" for gearman

--- lib/bipbip/plugin/gearman.rb
+++ lib/bipbip/plugin/gearman.rb
@@ -6,7 +6,8 @@ module Bipbip
   class Plugin::Gearman < Plugin
     def metrics_schema
       [
-        { name: 'jobs_queued_total', type: 'gauge', unit: 'Jobs' }
+        { name: 'jobs_queued_total', type: 'gauge', unit: 'Jobs' },
+        { name: 'jobs_active_total', type: 'gauge', unit: 'Jobs' }
       ]
     end
 
@@ -15,11 +16,16 @@ module Bipbip
       stats = gearman.status
 
       jobs_queued_total = 0
+      jobs_active_total = 0
       stats.each do |_function_name, data|
         jobs_queued_total += data[:queue].to_i
+        jobs_active_total += data[:active].to_i
       end
 
-      { jobs_queued_total: jobs_queued_total }
+      {
+        jobs_queued_total: jobs_queued_total,
+        jobs_active_total: jobs_active_total
+      }
     end
   end
 end
