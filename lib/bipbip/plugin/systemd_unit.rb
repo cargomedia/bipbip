4722afbcec3efdb0af61f63497ac23b7af2afa67
Tomasz Durka <tomasz@durka.pl>
2016-08-30 13:43:15 +0200

    Use Komenda, improve test coverage by stubbing komenda calls

--- lib/bipbip/plugin/systemd_unit.rb
+++ lib/bipbip/plugin/systemd_unit.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 require 'open3'
+require 'komenda'
 
 module Bipbip
   class Plugin::SystemdUnit < Plugin
@@ -13,39 +14,22 @@ module Bipbip
     def monitor
       data = Hash.new(0)
       main_unit = config['unit_name']
-      data['all_units_running'] = units(main_unit).all? { |unit| unit_is_running(unit) }
+      data['all_units_running'] = unit_dependencies(main_unit).all? { |unit| unit_is_active(unit) }
       data
     end
 
     # @param [String] main_unit
     # @return [Array<String>]
-    def units(main_unit)
-      command = "for unit in $(systemctl list-dependencies --plain --full #{main_unit}); do if [ \"${unit}\" != \"●\" ]; then echo \"${unit}\"; fi; done"
-      exec_command(command).split("\n")
+    def unit_dependencies(main_unit)
+      Komenda.run(['systemctl','list-dependencies', '--plain', '--full', main_unit]).stdout.split("\n").map do |line|
+        line.gsub(/^● /, '')
+      end
     end
 
     # @param [String] unit
     # @return [TrueClass, FalseClass]
-    def unit_is_running(unit)
-      exec_command("systemctl is-active --quiet #{unit}; echo $?").to_i == 0
-    end
-
-    def exec_command(command)
-      output_stdout = output_stderr = exit_code = nil
-      Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
-        output_stdout = stdout.read.chomp
-        output_stderr = stderr.read.chomp
-        exit_code = wait_thr.value
-      end
-
-      unless exit_code.success?
-        message = ['Command execution failed:', command]
-        message.push 'STDOUT:', output_stdout unless output_stdout.empty?
-        message.push 'STDERR:', output_stderr unless output_stderr.empty?
-        raise message.join("\n")
-      end
-
-      output_stdout
+    def unit_is_active(unit)
+      Komenda.run(['systemctl','is-active', unit]).success?
     end
   end
 end
--- spec/bipbip/plugin/systemd_unit_spec.rb
+++ spec/bipbip/plugin/systemd_unit_spec.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require 'bipbip'
 require 'bipbip/plugin/systemd_unit'
 
@@ -7,12 +9,28 @@ describe Bipbip::Plugin::SystemdUnit do
   end
 
   it 'should collect data' do
-    allow(plugin).to receive(:units).with('foo.target').and_return(%w(unit1 unit2))
-    allow(plugin).to receive(:unit_is_running).with('unit1').and_return(true)
-    allow(plugin).to receive(:unit_is_running).with('unit2').and_return(false)
+    allow(plugin).to receive(:unit_dependencies).with('foo.target').and_return(%w(unit1 unit2))
+    allow(plugin).to receive(:unit_is_active).with('unit1').and_return(true)
+    allow(plugin).to receive(:unit_is_active).with('unit2').and_return(false)
     plugin.monitor['all_units_running'].should eq(false)
 
-    allow(plugin).to receive(:unit_is_running).with('unit2').and_return(true)
+    allow(plugin).to receive(:unit_is_active).with('unit2').and_return(true)
     plugin.monitor['all_units_running'].should eq(true)
+  end
+
+  it 'should systemctl list-dependencies' do
+    result = double('result')
+    allow(result).to receive(:stdout).and_return("foo.target\n● foo-dependency1.service\n● foo-dependency1@5000.service\n")
+
+    allow(Komenda).to receive(:run).with(["systemctl", "list-dependencies", "--plain", "--full", "foo.target"]).and_return(result)
+    plugin.unit_dependencies('foo.target').should eq(%w(foo.target foo-dependency1.service foo-dependency1@5000.service))
+  end
+
+  it 'should systemctl is-active' do
+    result = double('result')
+    allow(result).to receive(:success?).and_return(true)
+
+    allow(Komenda).to receive(:run).with(["systemctl", "is-active", "bar.target"]).and_return(result)
+    plugin.unit_is_active('bar.target').should eq(true)
   end
 end
