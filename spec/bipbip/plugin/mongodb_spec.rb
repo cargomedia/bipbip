93b7b8e16d4c928c56f7c0c6c9ac0a445ae0a68a
Krzysztof Stasiak <me@krzysztof-stasiak.com>
2017-01-27 12:59:22 +0100

    Rubocop

--- spec/bipbip/plugin/mongodb_spec.rb
+++ spec/bipbip/plugin/mongodb_spec.rb
@@ -51,27 +51,27 @@ describe Bipbip::Plugin::Mongodb do
 
   it 'should collect slow queries' do
     plugin.stub(:fetch_server_status).and_return(
-        'repl' => {
-            'ismaster' => true
-        }
+      'repl' => {
+        'ismaster' => true
+      }
     )
 
     plugin.stub(:fetch_replica_status).and_return(
-        'set' => 'rep1',
-        'members' => [
-            { 'stateStr' => 'PRIMARY', 'optime' => BSON::Timestamp.new(1000, 1) },
-            { 'stateStr' => 'SECONDARY', 'optime' => BSON::Timestamp.new(1003, 1), 'self' => true }
-        ]
+      'set' => 'rep1',
+      'members' => [
+        { 'stateStr' => 'PRIMARY', 'optime' => BSON::Timestamp.new(1000, 1) },
+        { 'stateStr' => 'SECONDARY', 'optime' => BSON::Timestamp.new(1003, 1), 'self' => true }
+      ]
     )
 
     plugin.stub(:fetch_slow_queries_status).and_return(
-        'total' => {
-            'count' => 48.4,
-            'time' => 24.2
-        },
-        'max' => {
-            'time' => 12
-        }
+      'total' => {
+        'count' => 48.4,
+        'time' => 24.2
+      },
+      'max' => {
+        'time' => 12
+      }
     )
 
     plugin.stub(:total_index_size).and_return(50 * 1024 * 1024)
