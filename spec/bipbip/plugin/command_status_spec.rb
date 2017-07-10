728722bcea37e52e089812512c7678c876a8b2b7
Michel Meyer <michel.meyer@cargomedia.ch>
2017-07-10 15:13:41 +0200

    Use rspec "let" to set test instances

--- spec/bipbip/plugin/command_status_spec.rb
+++ spec/bipbip/plugin/command_status_spec.rb
@@ -5,10 +5,10 @@ require 'tempfile'
 describe Bipbip::Plugin::CommandStatus do
   command = 'echo "foo" > /dev/stdout; echo "bar" > /dev/stderr; exit 42;'
 
-  logger_file = Tempfile.new('bipbip-mock-logger')
-  logger = Logger.new(logger_file.path)
-  plugin = Bipbip::Plugin::CommandStatus.new('cmd-status', { 'command' => command }, 0.1)
-  agent = Bipbip::Agent.new(Bipbip::Config.new([plugin], [], logger))
+  let(:logger_file) { Tempfile.new('bipbip-mock-logger') }
+  let(:logger) { Logger.new(logger_file.path) }
+  let(:plugin) { Bipbip::Plugin::CommandStatus.new('cmd-status', { 'command' => command }, 0.1) }
+  let(:agent) { Bipbip::Agent.new(Bipbip::Config.new([plugin], [], logger)) }
 
   it 'should collect status:42' do
     thread = Thread.new { agent.run }
